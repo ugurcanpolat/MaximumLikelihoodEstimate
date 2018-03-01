d58b599e15794eea5b983673e3db36be2d90e806
ugurcanpolat <polatu@itu.edu.tr>
2018-03-02 02:03:38 +0300

    Add solution to Question 3with report and code

---
+++ mle_script.m
@@ -0,0 +1,35 @@
+% @Author
+% Student Name: Ugurcan Polat
+% Student ID : 150140117
+% Date: 02.03.2018
+
+fileID = fopen('data.txt','r'); % open file with read perm.
+[x, n] = fscanf(fileID,'%f'); % scan floating point data
+fclose(fileID); % close the file
+
+mu = 0;
+for count = 1:n % use equation for mu estimation
+    mu = mu + x(count);
+end
+mu = mu / n; % mean
+
+std = 0;
+for count = 1:n % use equation for deviation estimation
+    std = std + (x(count) - mu)^2;
+end
+std = std / n; 
+std = sqrt(std); % standard deviation
+
+MAX = max(x); % maximum value of given data
+MIN = min(x); % minimum value of given data
+x_e = linspace(MIN,MAX); % create a linear space
+y_e = (1/(sqrt(2*pi)*std))*exp(-0.5*((x_e-mu)/std).^2); % MLE using x_e
+
+figure 
+plot(x_e,y_e,'LineWidth',1.5) % plot with bold line
+hold on
+% Plot a histogram with normalization probability density function
+histogram(x,'Normalization','pdf') 
+xlim([0 18]) % x limit of the plot
+ylim([0 0.16]) % y limit of the plot
+hold off
